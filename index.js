04859a1d596f2c3fb15bdd1b498e4030a0211f40
Your Name <you@example.com>
2020-06-25 03:45:27 +0100

    save private information in dev.js

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const express = require('express');
 const app = express();
 //connect to mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Ebunoluwa:EbunAlabi20@react-blog.exzpc.mongodb.net/react-blog?retryWrites=true&w=majority',
+//add bodyparser
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+
+const {User} = require('./models/user');
+
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true})
 .then (()=> console.log('DB connected'))
 .catch(err=>console.error(err))
+
+ 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 //route handling
-app.get('/', (req, res)=>{
-    res.send('hello world')
-})
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body) 
+    //get data from user and send to mongo
+    user.save((err, userData)=>{
+        if (err) return res.json({success:false, err})
+    })
+    return res.status(200).json({success:true});
+
+});
 //listen at port
 app.listen(5000);
